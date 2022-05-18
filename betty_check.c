2d662a9744b1459e9889451dd6e0a6e41d0aea87
torpman <root@LAPTOP-7OCTDENM.localdomain>
2022-05-18 23:16:49 +0100

    betty will be happy

---
+++ betty_check.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - The program entry point
+*
+* Return: return 0 on success
+*/
+
+int main(void)
+{
+    int a, b, sum;
+
+    a = 33;
+    b = 45;
+    sum = a + b;
+
+    printf("The sum of %d and %d is = %d", a, b, sum);
+
+    return (0)
+}
